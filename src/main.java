e02e82c72843a97930041d6fdad3e1d53dc7024a
Betico1928 <betoveluis@gmail.com>
2023-02-10 08:34:25 -0500

    Estuvo divertido :D

--- src/main.java
+++ src/main.java
@@ -22,7 +22,6 @@ public class main
         {
             Matcher matcher = patron.matcher(buffer);
 
-            
             if (matcher.matches())
             {
                 System.out.println("La cadena: " + buffer+ " coincide con el lenguaje regular");
